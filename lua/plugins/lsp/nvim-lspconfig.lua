bf186c185a1402d1ae688fc106907b894f9f791b
CJYLZS <691086891@qq.com>
2023-06-04 14:55:00 +0000

    add python lsp config

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,4 +2,13 @@
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lspconfig = require 'lspconfig'
+        local on_attach = function(client)
+        end
+        -- python-lsp-server
+        lspconfig.pylsp.setup {
+            on_attach = on_attach
+        }
+    end
 }
